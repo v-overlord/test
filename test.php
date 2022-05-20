802686bec7cbc879651ea80bc514da73dd07e9b9
Vladislav Chizhmakov <v.chizhmakov@it.ecommpay.com>
2022-05-20 13:32:26 +0300

    CORE-0002: Added security check

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
 function send_money(int $user_id_from, int $user_id_to, float $amount): bool {
+    print("By admin's decision!");
     print("Log: send_to_user: $user_id_from -> $user_id_to [$amount]\n");
     print("Sending the money to user $user_id_to");
     return true;
 }
 
-send_money(42, 24, 100.42);
+if ($_ENV['is_admin'] ?? false) {
+    send_money(42, 24, 100.42);
+}
