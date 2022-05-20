451cd63d4261622887c645b4f9dd4dcc64cc47d6
Vladislav Chizhmakov <v.chizhmakov@it.ecommpay.com>
2022-05-20 13:44:42 +0300

    CORE-0004: Fixed

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 function send_money(int $user_id_from, int $user_id_to, float $amount): bool {
-    print("By admin's decision?..");
+    print("By admin's decision!..");
     print("Log: send_to_user: $user_id_from -> $user_id_to [$amount]\n");
     print("Sending the money to user $user_id_to");
     return true;
